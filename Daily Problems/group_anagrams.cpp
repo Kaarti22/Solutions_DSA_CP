32824c4d5bf7b30ed91f18d1229e7f5fa2891777
Kaarti22 <kaartimondi@gmail.com>
2024-02-06 19:06:51 +0530

    added group anagrams

--- Daily Problems/group_anagrams.cpp
+++ Daily Problems/group_anagrams.cpp
@@ -28,3 +28,43 @@ Constraints:
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.size() == 0){
+            return {{""}};
+        }
+        if(strs.size() == 1){
+            return {strs};
+        }
+        vector<pair<vector<int>,int>>v;
+        for(int i=0; i<strs.size(); i++){
+            vector<int>temp(26, 0);
+            for(auto j: strs[i]){
+                temp[j-'a']++;
+            }
+            v.push_back({temp, i});
+        }
+        sort(v.begin(), v.end());
+        vector<vector<string>>ans;
+        vector<string>res;
+        res.push_back(strs[v[0].second]);
+        for(int i=1; i<v.size(); i++){
+            if(v[i-1].first == v[i].first){
+                res.push_back(strs[v[i].second]);
+            } else {
+                ans.push_back(res);
+                res.clear();
+                res.push_back(strs[v[i].second]);
+            }
+            if(i == v.size()-1){
+                ans.push_back(res);
+            }
+        }
+        return ans;
+    }
+};
