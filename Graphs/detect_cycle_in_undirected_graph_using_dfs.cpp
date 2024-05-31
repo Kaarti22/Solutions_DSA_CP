a6d8862ab09b20b310b9d872586536fff2986b7c
Kaarti22 <kaartimondi@gmail.com>
2024-05-31 16:58:42 +0530

    detect cycle in undirected graph using dfs

---
+++ Graphs/detect_cycle_in_undirected_graph_using_dfs.cpp
@@ -0,0 +1,79 @@
+/*
+
+Link: https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+Given an undirected graph with V vertices labelled from 0 to V-1 and E edges, check whether it contains any cycle or not. Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.
+
+Example 1:
+
+Input:  
+V = 5, E = 5
+adj = {{1}, {0, 2, 4}, {1, 3}, {2, 4}, {1, 3}} 
+Output: 1
+Explanation: 
+
+1->2->3->4->1 is a cycle.
+Example 2:
+
+Input: 
+V = 4, E = 2
+adj = {{}, {2}, {1, 3}, {2}}
+Output: 0
+Explanation: 
+
+No cycle in the graph.
+ 
+
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isCycle() which takes V denoting the number of vertices and adjacency list as input parameters and returns a boolean value denoting if the undirected graph contains any cycle or not, return 1 if a cycle is present else return 0.
+
+NOTE: The adjacency list denotes the edges of the graph where edges[i] stores all other vertices to which ith vertex is connected.
+
+ 
+
+Expected Time Complexity: O(V + E)
+Expected Space Complexity: O(V)
+
+
+ 
+
+Constraints:
+1 ≤ V, E ≤ 105
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    
+  private:
+    
+    bool dfs(int node, int parent, vector<int>&vis, vector<int>adj[]){
+        vis[node] = 1;
+        for(auto i: adj[node]){
+            if(!vis[i]){
+                if(dfs(i, node, vis,adj)){
+                    return true;
+                }
+            } else if(parent != i){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>vis(V, 0);
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(dfs(i, -1, vis, adj)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
