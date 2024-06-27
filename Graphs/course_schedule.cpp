e7a130bc7bbf32453213a8318d0580f3388a7d9f
Kaarti22 <kaartimondi@gmail.com>
2024-06-27 13:11:15 +0530

    course schedule problem using kahn's algorithm

---
+++ Graphs/course_schedule.cpp
@@ -0,0 +1,71 @@
+/*
+Link: https://leetcode.com/problems/course-schedule/description/
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses, vector<int>());
+        for(auto it: prerequisites){
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<int>indegree(numCourses, 0);
+        for(int i=0; i<numCourses; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int c = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            c++;
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        if(c == numCourses) return true;
+        return false;
+    }
+};
