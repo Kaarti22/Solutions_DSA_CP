23dbc6e9b039ab349d7e53fe2a6423c363aa6cb5
Kaarti22 <kaartimondi@gmail.com>
2024-07-05 15:13:18 +0530

    finished daily problem on linked lists

---
+++ Trees/inorder.cpp
@@ -0,0 +1,64 @@
+/*
+Link: https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Recursive solution
+class Solution {
+private:
+    void inOrder(TreeNode* root, vector<int> &v){
+        if(!root) return;
+        inOrder(root->left, v);
+        v.push_back(root->val);
+        inOrder(root->right, v);
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        inOrder(root, v);
+        return v;
+    }
+};
+
+// Iterative solution
