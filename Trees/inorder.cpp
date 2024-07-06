08c13a54683116829fd5d7f943118abf1ec35532
Kaarti22 <kaartimondi@gmail.com>
2024-07-06 22:57:27 +0530

    inorder recursive and iterative

--- Trees/inorder.cpp
+++ Trees/inorder.cpp
@@ -62,3 +62,24 @@ public:
 };
 
 // Iterative solution
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        stack<TreeNode* >st;
+        TreeNode* node = root;
+        while(1){
+            if(node != nullptr){
+                st.push(node);
+                node = node->left;
+            } else {
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
+    }
+};
