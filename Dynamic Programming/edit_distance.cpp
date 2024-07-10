40b224c7604d561362d82a65822b7d375bb34191
Kaarti22 <kaartimondi@gmail.com>
2024-07-10 16:19:08 +0530

    edit distance

---
+++ Dynamic Programming/edit_distance.cpp
@@ -0,0 +1,64 @@
+/*
+Link: https://leetcode.com/problems/edit-distance/description/
+
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+ 
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+Example 2:
+
+Input: word1 = "intention", word2 = "execution"
+Output: 5
+Explanation: 
+intention -> inention (remove 't')
+inention -> enention (replace 'i' with 'e')
+enention -> exention (replace 'n' with 'x')
+exention -> exection (replace 'n' with 'c')
+exection -> execution (insert 'u')
+ 
+
+Constraints:
+
+0 <= word1.length, word2.length <= 500
+word1 and word2 consist of lowercase English letters.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1));
+        for(int i=0; i<n+1; i++){
+            dp[i][0] = i;
+        }
+        for(int i=1; i<m+1; i++){
+            dp[0][i] = i;
+        }
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
