162f7bbd77367adf29f41d864c99855a653829ae
Kaarti22 <kaartimondi@gmail.com>
2024-08-14 10:17:42 +0530

    climbing stairs

--- Dynamic Programming/climbing_stairs.cpp
+++ Dynamic Programming/climbing_stairs.cpp
@@ -35,12 +35,12 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-      int dp[n+1];
-      dp[0] = 1;
-      dp[1] = 1;
-      for(int i=2; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
-      }
-      return dp[n];
+        int dp[n+1];
+        if(n == 1) return 1;
+        dp[0] = 0, dp[1] = 1, dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
