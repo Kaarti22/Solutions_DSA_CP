e9958fcc509f7a3b51d0629d3c06d4cf7f09ff6d
Kaarti22 <kaartimondi@gmail.com>
2024-08-16 18:28:21 +0530

    krushkal's algorithm

---
+++ Graphs/krushkals_algorithm.cpp
@@ -0,0 +1,127 @@
+/*
+Link: https://www.geeksforgeeks.org/problems/minimum-spanning-tree/1
+
+Given a weighted, undirected, and connected graph with V vertices and E edges, your task is to find the sum of the weights of the edges in the Minimum Spanning Tree (MST) of the graph. The graph is represented by an adjacency list, where each element adj[i] is a vector containing vector of integers. Each vector represents an edge, with the first integer denoting the endpoint of the edge and the second integer denoting the weight of the edge.
+
+Example 1:
+
+Input:
+3 3
+0 1 5
+1 2 3
+0 2 1
+
+Output:
+4
+Explanation:
+
+The Spanning Tree resulting in a weight
+of 4 is shown above.
+Example 2:
+
+Input:
+2 1
+0 1 5
+
+Output:
+5
+Explanation:
+Only one Spanning Tree is possible
+which has a weight of 5.
+ 
+
+Your task:
+Since this is a functional problem you don't have to worry about input, you just have to complete the function spanningTree() which takes a number of vertices V and an adjacency list adj as input parameters and returns an integer denoting the sum of weights of the edges of the Minimum Spanning Tree. Here adj[i] contains vectors of size 2, where the first integer in that vector denotes the end of the edge and the second integer denotes the edge weight.
+
+Expected Time Complexity: O(ElogV).
+Expected Auxiliary Space: O(V2).
+ 
+
+Constraints:
+2 ≤ V ≤ 1000
+V-1 ≤ E ≤ (V*(V-1))/2
+1 ≤ w ≤ 1000
+The graph is connected and doesn't contain self-loops & multiple edges.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet{
+    vector<int>rank, parent, size;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0; i<n+1; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUltimateParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUltimateParent(parent[node]);
+    }
+
+    void UnionByRank(int u, int v){
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        } else if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void UnionBySize(int u, int v){
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        vector<pair<int, pair<int, int>>>edges;
+        for(int i=0; i<V; i++){
+            for(auto it: adj[i]){
+                int adjNode = it[0];
+                int edgeWeight = it[1];
+                int node = i;
+                edges.push_back({edgeWeight, {node, adjNode}});
+            }
+        }
+        sort(edges.begin(), edges.end());
+        int ans = 0;
+        DisjointSet ds(V);
+        for(auto it: edges){
+            int weight = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            if(ds.findUltimateParent(u) != ds.findUltimateParent(v)){
+                ans += weight;
+                ds.UnionByRank(u, v);
+            }
+        }
+        return ans;
+    }
+};
