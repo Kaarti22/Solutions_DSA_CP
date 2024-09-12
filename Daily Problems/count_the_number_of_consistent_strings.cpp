58375cb7b3e10e8f711a0f976f2f61c6dcf279d8
Kaarti22 <kaartimondi@gmail.com>
2024-09-13 00:21:26 +0530

    count the number of consistent strings

---
+++ Daily Problems/count_the_number_of_consistent_strings.cpp
@@ -0,0 +1,59 @@
+/*
+Link: https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ 
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        vector<int>v(26, 0);
+        for(auto it: allowed) v[it - 'a']++;
+        int ans = 0;
+        for(auto it: words){
+            vector<int>temp(26, 0);
+            bool f = 1;
+            for(auto i: it){
+                temp[i - 'a']++;
+                if(v[i - 'a'] == 0){
+                    f = 0;
+                    break;
+                }
+            }
+            if(f) ans++;
+        }
+        return ans;
+    }
+};
