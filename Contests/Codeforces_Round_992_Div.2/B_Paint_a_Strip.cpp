04303f4432e572a3689a1b8e5231f2556bbe055d
Kaarti22 <kaartimondi@gmail.com>
2024-12-10 16:34:30 +0530

    codeforces round 992 div 2 problems

---
+++ Contests/Codeforces_Round_992_Div.2/B_Paint_a_Strip.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(int n){
+        int ans = 1;
+        int curr = 1;
+        while(curr < n){
+            ans++;
+            curr = (curr + 1) * 2;
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int32_t main(){
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        Kaarti *ob = new Kaarti;
+        ob->method(n);
+    }
+    return 0;
+}
