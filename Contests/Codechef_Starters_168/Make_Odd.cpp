5b31de24684ec7617d5d33d4c9d469eeb0b62826
Kaarti22 <kaartimondi@gmail.com>
2025-01-09 10:25:46 +0530

    codechef starters 168

---
+++ Contests/Codechef_Starters_168/Make_Odd.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(string& s, string& t){
+        int c = 0, d = 0;
+        int n = s.size();
+        rep(i,0,n){
+            if(s[i] == t[i]){
+                if(s[i] == '1') c++;
+            } else {
+                d++;
+            }
+        }
+        if((c & 1) || d > 0){
+            cout<<"YES"<<endl;
+        } else {
+            cout<<"NO"<<endl;
+        }
+    }
+};
+
+int32_t main(){
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        Kaarti *ob = new Kaarti;
+        ob->method(a, b);
+    }
+    return 0;
+}
