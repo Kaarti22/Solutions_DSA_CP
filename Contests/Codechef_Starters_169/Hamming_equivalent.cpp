9ccb61918c6fcb16fa67ad243274046b274056d0
Kaarti22 <kaartimondi@gmail.com>
2025-01-16 15:56:44 +0530

    codechef starters 169

---
+++ Contests/Codechef_Starters_169/Hamming_equivalent.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi &v, int n){
+        map<int, set<int>>m;
+        vi count(n);
+        rep(i,0,n){
+            count[i] = __builtin_popcount(v[i]);
+            m[count[i]].insert(v[i]);
+        }
+        vi ans(n);
+        rep(i,0,n){
+            ans[i] = *m[count[i]].begin();
+            m[count[i]].erase(ans[i]);
+        }
+        rep(i,1,n){
+            if(ans[i] < ans[i-1]){
+                cout<<"No"<<endl;
+                return;
+            }
+        }
+        cout<<"Yes"<<endl;
+    }
+};
+
+int32_t main(){
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n;
+        cin>>n;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti *ob = new Kaarti;
+        ob->method(v, n);
+    }
+    return 0;
+}
