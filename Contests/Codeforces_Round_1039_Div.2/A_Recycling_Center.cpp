464050d44f01853c2ac5a7234ddd2dd47c97164e
Kaarti22 <kaartimondi@gmail.com>
2025-07-27 20:43:21 +0530

    codeforces round 1039 div 2

---
+++ Contests/Codeforces_Round_1039_Div.2/A_Recycling_Center.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define MOD 1000000007
+#define pb push_back
+#define popb pop_back
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define all(v) v.begin(),v.end()
+
+void inparr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+}
+
+void disarr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
+}
+
+void inpvec(vi &v, int n){
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+}
+
+void disvec(vi v, int n){
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<' ';
+    }
+    cout<<endl;
+}
+
+int madd(int a,int b) {
+    return (a+b)%MOD;
+}
+
+int msub(int a,int b){
+    return (((a-b)%MOD)+MOD)%MOD;
+}
+
+int mmul(int a,int b){
+    return ((a%MOD)*(b%MOD))%MOD;
+}
+
+class Kaarti{
+public:
+    void method(vi& v, int n, int c){
+        sort(all(v));
+        while(1){
+            auto it = lower_bound(all(v), c);
+            if(it == v.begin()) break;
+            it--;
+            v.erase(it);
+            n = v.size();
+            rep(i,0,n){
+                v[i] *= 2;
+            }
+        }
+        cout<<v.size()<<endl;
+    }
+};
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int testcase;
+    cin>>testcase;
+    for(int t=0; t<testcase; t++){
+        int n, c;
+        cin>>n>>c;
+        vi v(n);
+        inpvec(v, n);
+        Kaarti ob;
+        ob.method(v, n, c);
+    }
+    return 0;
+}
