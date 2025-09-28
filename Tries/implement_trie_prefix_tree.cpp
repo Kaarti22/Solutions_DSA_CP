ff76342a9a8a99c86ff46d6fe4fc7a5c59117267
Kaarti22 <kaartimondi@gmail.com>
2025-09-28 11:08:15 +0530

    implement trie (prefix tree)

---
+++ Tries/implement_trie_prefix_tree.cpp
@@ -0,0 +1,112 @@
+/*
+Link: https://leetcode.com/problems/implement-trie-prefix-tree/description/
+
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Trie {
+private:
+    struct Node {
+        Node* links[26];
+        bool flag = false;
+    };
+
+    Node* root;
+
+    bool containsKey(Node* node, char ch){
+        return node->links[ch - 'a'] != nullptr;
+    }
+
+    void put(Node* node, char ch, Node* newNode){
+        node->links[ch - 'a'] = newNode;
+    }
+
+    Node* get(Node* node, char ch){
+        return node->links[ch - 'a'];
+    }
+
+    void setEnd(Node* node){
+        node->flag = true;
+    }
+
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(auto ch: word){
+            if(!containsKey(node, ch)){
+                put(node, ch, new Node());
+            }
+            node = get(node, ch);
+        }
+        setEnd(node);
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(auto ch: word){
+            if(!containsKey(node, ch)){
+                return false;
+            }
+            node = get(node, ch);
+        }
+        return node->flag;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(auto ch: prefix){
+            if(!containsKey(node, ch)){
+                return false;
+            }
+            node = get(node, ch);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
